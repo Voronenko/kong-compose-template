920e32987e304785a369a508e624d7f900224786
Vyacheslav Voronenko <git@voronenko.info>
2020-03-16 19:21:09 +0100

    chore(example) attaching https certificate with tags

--- docs/load_certs.sh
+++ docs/load_certs.sh
@@ -3,7 +3,11 @@
 KONG="http://localhost:8001"
 SNIS="*.lvh.voronenko.net"
 
+set -xe
+
 curl -i -X POST $KONG/certificates \
     -F "cert=@./certs/fullchain.pem" \
     -F "key=@./certs/privkey.pem" \
-    -F "snis=${SNIS}"
+    -F "snis[]=*.lvh.voronenko.net" \
+    -F "tags[]=default" \
+    -F "tags[]=lvh"
